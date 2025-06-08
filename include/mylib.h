91391ee6c7a04371803562db88ba981ce6e6eeca
OBudiak <obudiak@kse.org.ua>
2025-06-08 14:58:51 +0300

    initial commit

---
+++ include/mylib.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+// Якщо збираємо бібліотеку, визначено MYLIB_EXPORTS → експорт
+#ifdef MYLIB_EXPORTS
+#  define MYLIB_API __declspec(dllexport)
+#else
+#  define MYLIB_API __declspec(dllimport)
+#endif
+
+// Щоб виключити манглінг імен у C++
+extern "C" {
+    // Приклад функції, яка буде у DLL
+    MYLIB_API int   add(int a, int b);
+    MYLIB_API void  hello();
+}
---
+++ src/mylib.cpp
@@ -0,0 +1,11 @@
+﻿#define MYLIB_EXPORTS    // щоб під час збірки цієї бібліотеки MYLIB_API = dllexport
+#include "mylib.h"
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+void hello() {
+    std::cout << "Hello from MyDll!\n";
+}
